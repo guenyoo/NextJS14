d5f7de186b14dbe40ae1e8c7c167b1177ecb5626
Örsan Günay <oersan.guenay@sportscheck.com>
2024-01-23 16:15:36 +0100

    Add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+import MainNav from '@/components/MainNav';
+
+export default function About() {
+  return (
+    <>
+      <MainNav />
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque iusto assumenda maxime hic rerum dolorem
+        blanditiis nisi fugit? Quas aperiam repellendus dolor in? Eius, minima provident culpa tempora delectus id?
+      </p>
+    </>
+  );
+}
