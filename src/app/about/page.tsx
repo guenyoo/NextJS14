826c5862f85e15d8a433b71456faedb646b14020
Örsan Günay <oersan.guenay@sportscheck.com>
2024-01-23 16:25:01 +0100

    Adjust layout to match home in about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,13 +2,13 @@ import MainNav from '@/components/MainNav';
 
 export default function About() {
   return (
-    <>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 pt-0">
       <MainNav />
       <h1>About</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque iusto assumenda maxime hic rerum dolorem
         blanditiis nisi fugit? Quas aperiam repellendus dolor in? Eius, minima provident culpa tempora delectus id?
       </p>
-    </>
+    </main>
   );
 }
