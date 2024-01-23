32ea253f65981d7fc0c364ae9bf8844e9e9cb730
Örsan Günay <oersan.guenay@sportscheck.com>
2024-01-23 16:15:47 +0100

    Use MainNav in Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image';
 import { fanwood, quattrocento } from './ui/fonts';
+import MainNav from '@/components/MainNav';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <MainNav />
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
@@ -41,7 +43,11 @@ export default function Home() {
       </div>
       <div>
         <h1 className={quattrocento.className}>Barlow-Montserrat</h1>
-        <p className={fanwood.className}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sit molestiae voluptates laborum veniam hic eius iure ex. Odio omnis consequuntur quam, magni reprehenderit aspernatur numquam. Nobis, dolores assumenda. Ratione.</p>
+        <p className={fanwood.className}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sit molestiae voluptates laborum veniam hic eius
+          iure ex. Odio omnis consequuntur quam, magni reprehenderit aspernatur numquam. Nobis, dolores assumenda.
+          Ratione.
+        </p>
       </div>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
