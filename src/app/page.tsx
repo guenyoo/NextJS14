8d8e2ca35570eb02b47126b6ec1e4a9b987bf71e
Örsan Günay <oersan.guenay@sportscheck.com>
2024-01-24 14:53:53 +0100

    Add Section with SVG Components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import Image from 'next/image';
 import { fanwood, quattrocento } from './ui/fonts';
 import MainNav from '@/components/MainNav';
+import MyNameIs from '@/components/svg/MyNameIs';
+import NiceToMeetYou from '@/components/svg/NiceToMeetYou';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 pt-0">
       <MainNav />
+      <section>
+        <MyNameIs />
+        <NiceToMeetYou />
+      </section>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
